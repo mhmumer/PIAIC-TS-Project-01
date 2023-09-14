222e5971d9f4f41add544b6e3cf841335e32beb9
Muhammad Umer <74130137+Umerrawala@users.noreply.github.com>
2023-09-14 19:51:19 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,43 @@
+#! /usr/bin/env node 
+import inquirer from "inquirer";
+async function calc() {
+    const answers = await inquirer.prompt([
+        {
+            type: "number",
+            name: "numberOne",
+            message: "Kindly enter your first no: "
+        },
+        {
+            type: "number",
+            name: "numberTwo",
+            message: "Kindly enter your second no: "
+        },
+        {
+            type: "list",
+            name: "operator",
+            choices: ["*", "+", "-", "/"],
+            message: "Select Operator: "
+        },
+    ]);
+    const { numberOne, numberTwo, operator } = answers;
+    if (numberOne && numberTwo && operator) {
+        let result = 0;
+        if (operator === "+") {
+            result = numberOne + numberTwo;
+        }
+        else if (operator === "-") {
+            result = numberOne - numberTwo;
+        }
+        if (operator === "/") {
+            result = numberOne / numberTwo;
+        }
+        if (operator === "*") {
+            result = numberOne * numberTwo;
+        }
+        console.log("Your result is :", result);
+    }
+    else {
+        console.log("Kindly enter valid input");
+    }
+}
+calc();
